efd8b9c2c369ea0c424ae303d942c6d578f580e6
Fazzie <1240419984@qq.com>
2021-03-02 22:59:00 +0800

    day2

---
+++ day1/zhizhen.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+# include <vector>
+# include <string>
+using namespace std;
+int main(){
+
+    int MyNumber[5];
+    int* pNumber = MyNumber;
+    cout << "pnumber = 0x" << hex << pNumber ;
+}
